c6a0f416fca9ef2c3306f02ea829ccd063e2d6c7
Chris Ware <c.p.ware@hotmail.com>
2025-06-10 20:27:59 -0700

    nav fix

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -61,7 +61,7 @@ export default function Nav() {
           <Logo />
         </div>
 
-        <div className={styles.navItems} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.navItems}>
           {navItems.map((item) => (
             <Link key={item.href} href={item.href} className={styles.navItem}>
               {item.text}
